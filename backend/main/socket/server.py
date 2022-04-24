969d3f828a24f4e85ec2ad986711129153accbe2
Kevin Farias <kevin.farias.dev@gmail.com>
2022-04-24 11:34:01 -0300

    feat: first implementation of the client-socket connection

--- backend/main/socket/server.py
+++ backend/main/socket/server.py
@@ -2,6 +2,17 @@ import asyncio
 
 import websockets
 
+import os
+
+import sys
+
+from predictor import Predictor
+
+import base64
+
+from PIL import Image 
+
+predictor = Predictor(os.path.abspath("../../models/other_models/model_epoch_48_98.6_final.h5"))
 # create handler for each connection
 
 async def handler(websocket, path):
@@ -11,7 +22,16 @@ async def handler(websocket, path):
         try:
             data = await websocket.recv()
 
-            reply = f"{i}: Data received as: {data}!"
+            try:
+                img_src = os.path.abspath("../../temp/img.png")
+                
+                with open(img_src, "wb") as fh:
+                    fh.write(base64.urlsafe_b64decode(data[22:]))
+
+                img_text = predictor.predict(img_src)[1]
+                reply = f"{i}: Data received as: {img_text}!"
+            except Exception as e:
+                reply = f"{i}: Data received as: {e}!"
 
             await websocket.send(reply)
 
