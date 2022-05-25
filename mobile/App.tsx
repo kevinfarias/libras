666d533a1ec522377c6613e57d2b9efe366d5e86
kevin farias <kevin.farias.dev@gmail.com>
2022-05-25 13:54:16 -0300

    build(backend): add docker image

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import mainStyles from './styles/main';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(CameraType.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={mainStyles.container}>
+      <Camera style={mainStyles.camera} type={type}>
+        <View style={mainStyles.buttonContainer}>
+          <TouchableOpacity
+            style={mainStyles.button}
+            onPress={() => {
+              setType(type === CameraType.back ? CameraType.front : CameraType.back);
+            }}>
+            <Text style={mainStyles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/styles/main.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'transparent',
+    },
+    camera: {
+        height: '100%',
+    },
+    buttonContainer: {
+        height: '20%'
+    },
+    button: {
+        height: '100%'
+    }
+}); 
+
+export default styles;
